2bbc41209ff8439047c3f1406974626c1a98e534
Joerg Beekmann <jbeeko>
2016-12-20 09:52:27 -0800

    tidy test script

--- src/FSONParser/Script.fsx
+++ src/FSONParser/Script.fsx
@@ -7,8 +7,6 @@
 open System
 open System.IO
 open FParsec
-open System.Net
-open System.Net.Mail
 open FSONParser
 open Models
 
@@ -16,18 +14,13 @@ let test p str =
     match run p str with
     | Success(result, _, _)   -> result
     | Failure(errorMsg, _, _) -> failwith (sprintf "Failure: %s" errorMsg)
-// let toType t anObj = anObj :?> 
-
-// let pcontract : Parser<Contract,unit> = 
-//     let toType (anObj : obj) : Contract =
-//         anObj :?> Contract
-//     ptype typeof<Contract> |>> toType
 
 let contractData = File.ReadAllText("./src/FSONParser/SampleData.fson")
 
 let parsed = test (ptype typeof<Contract> |>> (fun anObj -> anObj :?> Contract)) contractData
 parsed = SampleModels.constructed
 parsed.Jurisdiction = BC
-
-
-
+match parsed.Holder with
+| Person p -> sprintf "Holder is %s" p.Name
+| Company c -> sprintf "Holder company %s" c.Name
+| Tag t -> sprintf "Holder is tag %s" t
