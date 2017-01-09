5820e0e18aaf858fd6301318be4654d1898747be
Joerg Beekmann <jbeeko>
2017-01-08 16:12:49 -0800

    *

--- src/FSONParser/Script.fsx
+++ src/FSONParser/Script.fsx
@@ -24,19 +24,3 @@ Country: Canada"
 let addr = (parseFSON typeof<Address> data) :?> Address
 
 
-type DU = 
-    | One | Two | Three
-
-
- ((parseFSON typeof<DU list> "One Two") :?> DU list)
-
-
- charsTillString "bar" false 100>>.pstring "bar"
-
-
- let test p str =
-    match run p str with
-    | Success(result, _, _)   -> printfn "Success: %A" result
-    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
-
-test (charsTillString "bar" false 100 .>> pstring "bar") "foobar"
